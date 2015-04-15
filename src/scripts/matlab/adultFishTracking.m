d04740722b37e5677864bc3627e32d4312d74e2c
ardekani <dehestan@usc.edu>
2015-04-15 10:30:11 -0700

    added more comments

--- src/scripts/matlab/adultFishTracking.m
+++ src/scripts/matlab/adultFishTracking.m
@@ -1,74 +1,70 @@
-% TODO:
-% get ROI as input arguement
-% get stepSize as input arguement
-% process all ROI's with one readframe command
-
-% ROI naming format is :
-%  1     2    3  ...n
-% n+1   n+2  n+3 ...2n
-% so with four - the order should be topleft, topright, bottomleft,
-% bottomright
-
-
+% Author: Reza Ardekani
+% Date  : Feb 2015
+% Input:
+       %pathToTif : the path to the directory  
+       %firstFrame 
+       %lastFrame
+       %roiList: this is a list of roi objects: (roi.x, roi.y) is the top
+       %left corner and roi.width and roi.height are width and height
+ 
+% Output:
+       % one csv file for each roi
+       % ROI naming format is :
+        %  1     2    3  ...n
+        % n+1   n+2  n+3 ...2n
+        % so with four - the order should be topleft, topright, bottomleft,
+        % bottomright
+        %output file name is allInfo_roi_{roi_number},'.csv' and it's
+        %saved in the same directory as tiff files.
+       
+%TODO:
+% Get stepSize as input arguement
+% Get threshold as input arguement
+% Sanity check of input arguements
+% TO increase the speed: process all ROI's with one readframe command
 
 function adultFishTracking(pathToTif, firstFrame, lastFrame, roiList)
 
-stepSize = 100;
-thresh = 50;
-% roiLeft.x = 1;
-% roiLeft.y = 1;
-% roiLeft.width= 324;
-% roiLeft.height = 344;
-% 
-% roiRight.x = 335;
-% roiRight.y = 1;
-% roiRight.width = 362;
-% roiRight.height = 344;
-% 
-% roiList = {roiLeft, roiRight};
-for rr = 1:length(roiList)
-    
-    roi= roiList{rr};
-    bg = returnBackgroundModel(pathToTif, roi, firstFrame,lastFrame,stepSize);
+    stepSize = 100;
+    thresh = 50;
 
-    allInfo = zeros(lastFrame-firstFrame + 1,5);
-    for fn = firstFrame:lastFrame
-        img = readFrame(fn, pathToTif, roi);
-        changeMask = uint16(bg - double(img));
-%         imshow(changeMask,[]);
-%         pause(0.2);
-        changeMaskThresh = zeros(size(changeMask));
-        %TODO: make this threshold accessable
-        changeMaskThresh(find(changeMask>thresh)) = 1;
-        
-        [x, y] = find(changeMaskThresh>0);
-        xPos = floor(mean(x));
-        yPos = floor(mean(y));
-        allInfo(fn-firstFrame+1,1) = fn;
-        allInfo(fn-firstFrame+1,2) = 1;
-        allInfo(fn-firstFrame+1,3) = xPos;
-        allInfo(fn-firstFrame+1,4) = yPos;
-        allInfo(fn-firstFrame+1,5) = floor(mean2(img));
-%         imshow(changeMaskThresh,[]);
-%         hold on;
-%         plot(allInfo(fn,4), allInfo(fn,3),'o', 'linewidth',2);
-%         pause(0.2);
-%         hold off;
-        if ~(mod(fn,1000))
-        display(fn)
+    for rr = 1:length(roiList)
+
+        roi= roiList{rr};
+        bg = returnBackgroundModel(pathToTif, roi, firstFrame,lastFrame,stepSize);
+
+        allInfo = zeros(lastFrame-firstFrame + 1,5);
+        for fn = firstFrame:lastFrame
+            img = readFrame(fn, pathToTif, roi);
+            changeMask = uint16(bg - double(img));
+    %         imshow(changeMask,[]);
+    %         pause(0.2);
+            changeMaskThresh = zeros(size(changeMask));
+            %TODO: replace find with logical index to improve the speed
+            changeMaskThresh(find(changeMask>thresh)) = 1;
+
+            [x, y] = find(changeMaskThresh>0);
+            xPos = floor(mean(x));
+            yPos = floor(mean(y));
+            allInfo(fn-firstFrame+1,1) = fn;
+            allInfo(fn-firstFrame+1,2) = 1;
+            allInfo(fn-firstFrame+1,3) = xPos;
+            allInfo(fn-firstFrame+1,4) = yPos;
+            allInfo(fn-firstFrame+1,5) = floor(mean2(img));
+    %         imshow(changeMaskThresh,[]);
+    %         hold on;
+    %         plot(allInfo(fn,4), allInfo(fn,3),'o', 'linewidth',2);
+    %         pause(0.2);
+    %         hold off;
+            if ~(mod(fn,1000))
+                display(fn)
+            end
         end
+
+        roi_text = strcat('roi_',num2str(rr));
+        csvwrite(strcat(pathToTif, '/allInfo_',roi_text,'.csv'), allInfo);
+
     end
-%     roi_text = strcat(num2str(roi.x),'_',num2str(roi.y),'_',num2str(roi.width),'_', num2str(roi.height));
-%     if (rr==1)
-%         roi_text='leftChamberNew';
-%     else
-%         roi_text = 'rightChamberNew';
-%     end
-    roi_text = strcat('roi_',num2str(rr));
-    
-    csvwrite(strcat(pathToTif, '/allInfo_',roi_text,'.csv'), allInfo);
-    
-end
 
 end
 
--- src/scripts/matlab/returnBackgroundModel.m
+++ src/scripts/matlab/returnBackgroundModel.m
@@ -1,20 +1,25 @@
 % Author: Reza Ardekani
 % Date: Feb 2015
-% Function: returns background model from tif image sequences..
-% TODO: Params..
-% TODO: have a better return value!ß
+% Function: returns background model from tif image sequences using
+% sample frames from the first frame to the last frame with step size of
 
+% Input:
+       %pathToTif : the path to the directory
+       %roi: processing region of the frame 
+       %firstFrame 
+       %lastFrame
+       %stepSize: the distance between sampled frame
+% Output:
+        %bg: background model, a matrix with same size of the roi
+       
 function [ bg ] = returnBackgroundModel( pathToTif, roi, firstFrame, lastFrame, stepSize)
     frameNumbers = firstFrame:stepSize:lastFrame;
     bg = readFrame(frameNumbers(1),pathToTif,roi);
     count = 1;
-    for ii = 2:length(frameNumbers)
-        
+    for ii = 2:length(frameNumbers) 
         curFrame = readFrame(frameNumbers(ii),pathToTif,roi);
         bg = (count*double(bg) + double(curFrame))./(count+1);
        count = count+1;     
     end
-
-
 end
 
